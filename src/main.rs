4204b562cd40d09e650512cb3b3f94db75b2ed75
m-88888888 <mochi.h8888@gmail.com>
2024-05-11 19:45:34 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let entries = fs::read_dir("json")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    // println!("{:?}", entries);
+
+    for entry in entries {
+        println!("{:?}", entry);
+
+        let contents = fs::read_to_string(entry)?;
+
+        println!("{:?}", contents);
+    }
+
+    Ok(())
 }
